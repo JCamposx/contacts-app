f16efe565129c9455987ab64c5139917295e77d6
Jesus Campos <jesus.campos123@icloud.com>
2022-12-09 15:55:31 -0500

    frontend: navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          <img
+            src="/logo.png"
+            alt="Logo"
+            width="24"
+            height="24"
+            className="d-inline-block"
+            style={{ marginInlineEnd: "5px" }}
+          />
+          Contacts App
+        </a>
+      </div>
+    </nav>
+  );
+}
