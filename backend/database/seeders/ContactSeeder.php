413c869fd0423d6c9a0d74e841a629d657d57d30
Jesus Campos <jesus.campos123@icloud.com>
2022-12-09 21:46:36 -0500

    backend: add contact seeder

---
+++ backend/database/seeders/ContactSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Contact;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ContactSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Contact::factory()->count(10)->create();
+    }
+}
