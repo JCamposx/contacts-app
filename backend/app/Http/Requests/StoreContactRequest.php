00ac61614eb0e9a5740610269c8f00402a59e231
Jesus Campos <jesus.campos123@icloud.com>
2022-12-12 16:30:19 -0500

    backend: throw error on failed request validation

--- backend/app/Http/Requests/StoreContactRequest.php
+++ backend/app/Http/Requests/StoreContactRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class StoreContactRequest extends FormRequest
 {
@@ -28,5 +30,12 @@ class StoreContactRequest extends FormRequest
             'description' => 'required|string',
             'phone_number' => 'required|numeric|digits:9',
         ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'errors' => $validator->errors(),
+        ], 422));
     }
 }
