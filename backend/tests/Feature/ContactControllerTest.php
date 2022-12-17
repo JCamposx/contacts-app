9074c9ae221183d4db33fdd118417473b5b0d82c
Jesus Campos <jesus.campos123@icloud.com>
2022-12-17 16:20:24 -0500

    backend: refactor tests of contact controller

--- backend/tests/Feature/ContactControllerTest.php
+++ backend/tests/Feature/ContactControllerTest.php
@@ -5,7 +5,6 @@ namespace Tests\Feature;
 use App\Models\Contact;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ContactControllerTest extends TestCase
@@ -21,10 +20,12 @@ class ContactControllerTest extends TestCase
     {
         $user = User::factory()->create();
 
-        $contact = Contact::factory()->makeOne();
+        $contact = Contact::factory()->makeOne([
+            'user_id' => $user->id,
+        ]);
 
         $this
-            ->actingAs($user)
+            ->actingAs($user, 'api')
             ->post(route('contacts.store'), $contact->getAttributes());
 
         $this->assertDatabaseCount('contacts', 1);
@@ -37,20 +38,28 @@ class ContactControllerTest extends TestCase
     }
 
     /**
-     * Test api can show specific contact.
+     * Test api can show specific contact only to its owner.
      *
      * @return void
      */
-    public function test_api_can_return_contact()
+    public function test_only_owner_can_see_his_contacts()
     {
-        $user = User::factory()->create();
+        [$owner, $notOwner] = User::factory(2)->create();
 
-        $contact = Contact::factory()->createOne();
+        $contact = Contact::factory()->createOne([
+            'user_id' => $owner->id,
+        ]);
 
         $this->assertDatabaseCount('contacts', 1);
 
         $response = $this
-            ->actingAs($user)
+            ->actingAs($notOwner, 'api')
+            ->get(route('contacts.show', $contact->id));
+
+        $response->assertNotFound();
+
+        $response = $this
+            ->actingAs($owner, 'api')
             ->get(route('contacts.show', $contact->id));
 
         $response->assertJson([
@@ -62,20 +71,32 @@ class ContactControllerTest extends TestCase
     }
 
     /**
-     * Test api can update specific contact.
+     * Test api can update specific contact only if request belongs to owner.
      *
      * @return void
      */
-    public function test_api_can_update_contacts()
+    public function test_only_owner_can_update_his_contacts()
     {
-        $user = User::factory()->create();
+        [$owner, $notOwner] = User::factory(2)->create();
 
-        $contact = Contact::factory()->createOne();
+        $contact = Contact::factory()->createOne([
+            'user_id' => $owner->id,
+        ]);
 
         $this->assertDatabaseCount('contacts', 1);
 
-        $this
-            ->actingAs($user)
+        $response = $this
+            ->actingAs($notOwner, 'api')
+            ->put(route('contacts.update', $contact->id), [
+                "name" => "Name 1",
+                "description" => "Description 1",
+                "phone_number" => "123456789",
+            ]);
+
+        $response->assertNotFound();
+
+        $response = $this
+            ->actingAs($owner, 'api')
             ->put(route('contacts.update', $contact->id), [
                 "name" => "Name 1",
                 "description" => "Description 1",
@@ -93,20 +114,33 @@ class ContactControllerTest extends TestCase
     }
 
     /**
-     * Test api can delete specific contact.
+     * Test api can delete specific contact only if request belongs to owner.
      *
      * @return void
      */
-    public function test_api_can_delete_contacts()
+    public function test_only_owner_can_delete_his_contacts()
     {
-        $user = User::factory()->create();
+        [$owner, $notOwner] = User::factory(2)->create();
 
-        $contact = Contact::factory()->createOne();
+        $contact = Contact::factory()->createOne([
+            'user_id' => $owner->id,
+        ]);
 
         $this->assertDatabaseCount('contacts', 1);
 
-        $this
-            ->actingAs($user)
+        $response = $this
+            ->actingAs($notOwner, 'api')
+            ->delete(
+                route('contacts.destroy', $contact->id),
+                $contact->getAttributes()
+            );
+
+        $response->assertNotFound();
+
+        $this->assertDatabaseCount('contacts', 1);
+
+        $response = $this
+            ->actingAs($owner, 'api')
             ->delete(
                 route('contacts.destroy', $contact->id),
                 $contact->getAttributes()
