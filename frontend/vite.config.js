0740f38dc5830bee1d038c1007b0c87c7a9bf2d8
Jesus Campos <jesus.campos123@icloud.com>
2022-12-18 00:50:59 -0500

    dockerize app

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,8 @@ import { defineConfig } from "vite";
 export default defineConfig({
   resolve: { alias: { "@": "/src" } },
   plugins: [react()],
+  server: {
+    host: true,
+    port: 8080,
+  },
 });
