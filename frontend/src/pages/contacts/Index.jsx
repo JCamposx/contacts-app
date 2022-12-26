21a6808bbb45ad0c09f5e84f1690279a2068202b
Jesus Campos <jesus.campos123@icloud.com>
2022-12-25 23:09:07 -0500

    frontend: add search bar on contacts index page

--- frontend/src/pages/contacts/Index.jsx
+++ frontend/src/pages/contacts/Index.jsx
@@ -1,14 +1,18 @@
 import colorType from "@/assets/js/colorType";
 import Alert from "@/components/Alert";
+import Input from "@/components/Input";
 import Spinner from "@/components/Spinner";
 import { AlertContext } from "@/context/AlertContext";
 import { useContact } from "@/hooks/useContact";
 import { useFlashMessage } from "@/hooks/useFlashMessage";
 import ContactList from "@/views/ContactList";
 import NoContact from "@/views/NoContact";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 
 export default function Index() {
+  const [contacts, setContacts] = useState([]);
+  const [search, setSearch] = useState("");
+
   const { showAlert, hideAlert } = useContext(AlertContext);
 
   const {
@@ -35,9 +39,29 @@ export default function Index() {
     }
   }, [flashMessage]);
 
+  useEffect(() => {
+    setContacts([...data]);
+  }, [data]);
+
+  useEffect(() => {
+    setContacts(
+      data.filter((contact) => contact.name.toLowerCase().includes(search))
+    );
+  }, [search]);
+
   return (
     <>
-      <h1>Contacts</h1>
+      <div className="d-flex justify-content-between align-items-center">
+        <h1>Contacts</h1>
+        <div className="col-5">
+          <Input
+            placeholder="Search"
+            type="text"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </div>
+      </div>
       {isLoading ? (
         <Spinner />
       ) : error !== "" ? (
@@ -51,7 +75,7 @@ export default function Index() {
               <NoContact />
             ) : (
               <ContactList
-                contacts={data}
+                contacts={contacts}
                 onDeleteContact={deleteContact}
                 customClass="col-md-6"
               />
